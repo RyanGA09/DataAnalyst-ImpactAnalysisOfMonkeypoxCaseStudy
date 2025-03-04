43a28645ea769f8a734f529d5f3e4fbeae4b5814
Ryan <ryangadinga90@gmail.com>
2025-03-04 10:59:25 +0700

    update README, data, python & notebook

--- data/README.md
+++ data/README.md
@@ -10,7 +10,7 @@ ImpactAnalysisOfMonkeypoxCaseStudy/data
 ├── processed/                                                                            # Contains the processed data, used for EDA and visualization.
 │   └── monkeypox_{start_year}_{start_month}_to_{end_year}_{end_month}_processed.csv      # Processed Data file
 ├── raw/                                                                                  # Raw dataset folder
-│     ├── filtered                                                                        # Contains the filtered data, ready for analysis.
+│     ├── filtered/                                                                       # Contains the filtered data, ready for analysis.
 │     │   └── monkeypox_{start_year}_{start_month}_to_{end_year}_{end_month}_filtered.csv # Raw filtered data file
 │     └── original/                                                                       # Contains the original, unprocessed data.
 │         └── monkeypox_{start_year}_{start_month}_to_{end_year}_{end_month}.csv          # Raw original data file
--- filter_monkeypox_data.py
+++ filter_monkeypox_data.py
@@ -49,11 +49,19 @@ else:
             new_start_year = int(input("Enter the new start year for saving (e.g., 2022): "))
             new_start_month = int(input("Enter the new start month for saving (1-12): "))
             
+            # Input for new end year and month for saving
+            new_end_year = int(input("Enter the end year (e.g., 2024): "))
+            new_end_month = int(input("Enter the end month (1-12): "))
+            
             # Validate the new start year and month input
-            if new_start_month < 1 or new_start_month > 12:
+            if new_start_month < 1 or new_start_month > 12 or new_end_month < 1 or new_end_month > 12:
                 print("Month must be between 1 and 12. Please try again.")
             elif new_start_year < start_year or (new_start_year == start_year and new_start_month < start_month):
                 print("The new start date for saving cannot be earlier than the original start date. Please try again.")
+            elif new_end_year < new_start_year or (new_end_year == new_start_year and new_end_month < new_start_month):
+                print("The new end date for saving cannot be earlier than the new start date. Please try again.")
+            elif new_end_year > end_year or (new_end_year == end_year and new_end_month > end_month):
+                print("The new end date for saving cannot be later than the original end date. Please try again.")
             else:
                 break
         except ValueError:
@@ -64,7 +72,7 @@ else:
     os.makedirs(output_folder, exist_ok=True)  # Create folder if it doesn't exist
 
     # Save the filtered data to a new file in the filtered folder with new start year and month for saving
-    output_file = os.path.join(output_folder, f'monkeypox_{new_start_year}_{new_start_month}_to_{end_year}_{end_month}_filtered.csv')
+    output_file = os.path.join(output_folder, f'monkeypox_{new_start_year}_{new_start_month}_to_{new_end_year}_{new_end_month}_filtered.csv').replace("\\", "/")
     filtered_data.to_csv(output_file, index=False)
 
     print(f"Data successfully filtered for the range {start_month}/{start_year} to {end_month}/{end_year}, and saved to: {output_file}")
